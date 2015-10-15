481bfef99eb6df9e50cb4a684a4b2c8b8a9daaa2
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-10-15 16:19:28 -0700

    add LICENSE

--- json_fixtures.js
+++ json_fixtures.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2014, Yahoo Inc. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
 var camelize = require('./camelize');
 
 module.exports = (function () {
